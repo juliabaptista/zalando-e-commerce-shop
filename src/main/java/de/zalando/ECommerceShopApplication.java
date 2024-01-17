1f98b227b02dc4a91610e299914356ed084b3e60
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-17 10:44:23 +0100

    [PXZ-000] - ECommerceShopApplication
    - Add annotations, such as:
    	@EntityScan
    	@EnableJpaRepositories

--- src/main/java/de/zalando/ECommerceShopApplication.java
+++ src/main/java/de/zalando/ECommerceShopApplication.java
@@ -2,12 +2,17 @@ package de.zalando;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EntityScan("de.zalando.model.entities")
+@EnableJpaRepositories("de.zalando.model.repositories")
 @RestController
 @OpenAPIDefinition(info = @Info(
 		title = "Zalando E-commerce Shop",
@@ -19,10 +24,4 @@ public class ECommerceShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceShopApplication.class, args);
 	}
-
-	@GetMapping(path = "/")
-	public String greetings() {
-		return "Hello World";
-	}
-
 }
