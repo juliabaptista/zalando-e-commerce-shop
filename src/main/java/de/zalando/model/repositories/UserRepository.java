f9b1df6647d05a9f0e08fd0fb4ac8d35e252080e
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-17 10:43:38 +0100

    [PXZ-58] - Create User repository

---
+++ src/main/java/de/zalando/model/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package de.zalando.model.repositories;
+
+import de.zalando.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
