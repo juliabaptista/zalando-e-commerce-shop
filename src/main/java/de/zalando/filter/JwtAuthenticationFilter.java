20e2334d8dbe8fcc4fb86779f127a5f1359d1998
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-18 15:43:41 +0100

    [PXZ-32] - Create Jwt Authentication Filter

---
+++ src/main/java/de/zalando/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package de.zalando.filter;
+
+import de.zalando.helper.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  protected void doFilterInternal(
+      @NotNull HttpServletRequest request,
+      @NotNull HttpServletResponse response,
+      @NotNull FilterChain filterChain
+  ) throws ServletException, IOException {
+
+    final String authHeader = request.getHeader("Authorazation");
+    final String jwt;
+    final String username;
+
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    jwt = authHeader.substring(7);
+    username = jwtTokenProvider.extractsUsername(jwt);
+
+  }
+}
