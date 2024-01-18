b42a30758d80ed06f243c3ee6de032849787a09a
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-18 20:07:18 +0100

    [PXZ-32] - Create Jwt Authentication Filter

--- src/main/java/de/zalando/filter/JwtAuthenticationFilter.java
+++ src/main/java/de/zalando/filter/JwtAuthenticationFilter.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -40,6 +42,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     userEmail = jwtTokenProvider.extractUserName(jwt);
     if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
       UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+      if (jwtTokenProvider.isTokenValid(jwt, userDetails)) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            userDetails,
+            null,
+            userDetails.getAuthorities()
+        );
+        authenticationToken.setDetails(
+            new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
     }
+    filterChain.doFilter(request, response);
   }
 }
