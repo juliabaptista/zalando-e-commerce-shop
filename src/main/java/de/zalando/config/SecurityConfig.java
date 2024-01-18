b6081403217e4a7c6a7a497043eca91256c77996
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-18 21:07:24 +0100

    [PXZ-29] - Configure Spring Security
    Implement:
    - securityFilterChain():  is a chain of filters that are responsible for processing and managing security-related tasks in a web application.
    - authenticationProvider(): this method is used to define and configure an AuthenticationProvider bean for authentication.
    	- add passwordEncoder() to ApplicationConfig file
    - delete PasswordEncoder class to not have redundance

---
+++ src/main/java/de/zalando/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package de.zalando.config;
+
+import de.zalando.filter.JwtAuthenticationFilter;
+import de.zalando.service.UserService;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    http
+        .csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .requestMatchers("")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+
+
+    return http.build();
+  }
+
+}
