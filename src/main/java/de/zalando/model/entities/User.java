dc1f4f8551ccaddf966785b6c00dc9acfed2f594
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-18 20:28:58 +0100

    [PXZ-000] - extends UserDetails in User entity
    The use of UserDetails in Spring Security is fundamental to how authentication and authorization are implemented.

--- src/main/java/de/zalando/model/entities/User.java
+++ src/main/java/de/zalando/model/entities/User.java
@@ -2,12 +2,17 @@ package de.zalando.model.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.List;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "user")
 @Setter @Getter @NoArgsConstructor @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
   @Id
   @Column(name = "user_id")
@@ -25,4 +30,33 @@ public class User {
   @Column(name = "role_id")
   private Role role;
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
