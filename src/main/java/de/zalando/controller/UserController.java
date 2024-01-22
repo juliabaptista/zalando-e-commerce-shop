f6d32edfeacd7d566048d1055e333019a41190cf
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-22 15:13:51 +0100

    [PXZ-65] - User Controller
    Implement:
    	- method registerUser

---
+++ src/main/java/de/zalando/controller/UserController.java
@@ -0,0 +1,34 @@
+package de.zalando.controller;
+
+import de.zalando.dto.UserRegistrationRequest;
+import de.zalando.dto.UserRegistrationResponse;
+import de.zalando.exception.DuplicateUserException;
+import de.zalando.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+  UserService userService;
+
+  @PostMapping("/register")
+  public ResponseEntity<?> registerUser(
+      @RequestBody UserRegistrationRequest registrationRequest
+  ) {
+    try {
+      UserRegistrationResponse response = userService.registerUser(registrationRequest);
+      return ResponseEntity.ok(response);
+
+    } catch (DuplicateUserException e) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+    }
+  }
+}
