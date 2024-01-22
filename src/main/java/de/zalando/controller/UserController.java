89eb3322188c908b45ab2f90003a179b80833b06
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-22 20:22:24 +0100

    [PXZ-65] - User Controller
    Implement:
    	- login endpoint
    	- add injections: AuthenticationManager and JwtTokenProvider
    	- implement authenticate method from authenticationManager
    	- upon a successful login -> generate jwt
    	- ensure there is an AuthenticationException for invalid credentials

--- src/main/java/de/zalando/controller/UserController.java
+++ src/main/java/de/zalando/controller/UserController.java
@@ -1,13 +1,19 @@
 package de.zalando.controller;
 
+import de.zalando.dto.AuthenticationResponse;
+import de.zalando.dto.LoginRequest;
 import de.zalando.dto.UserRegistrationRequest;
 import de.zalando.dto.UserRegistrationResponse;
 import de.zalando.exception.DuplicateUserException;
+import de.zalando.helper.JwtTokenProvider;
 import de.zalando.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
   UserService userService;
+  AuthenticationManager authenticationManager;
+  JwtTokenProvider jwtTokenProvider;
 
   @Autowired
   public UserController(UserService userService) {
@@ -35,6 +43,26 @@ public class UserController {
 
     } catch (DuplicateUserException e) {
       return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+    }
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(
+      @RequestBody LoginRequest loginRequest
+  ) {
+    try {
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+              loginRequest.getEmail(),
+              loginRequest.getPassword()
+          )
+      );
+
+      final String jwt = jwtTokenProvider.generateToken(loginRequest.getEmail());
+      return ResponseEntity.ok(new AuthenticationResponse(jwt, "Successful login!"));
+
+    } catch (AuthenticationException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials.");
     }
   }
 }
