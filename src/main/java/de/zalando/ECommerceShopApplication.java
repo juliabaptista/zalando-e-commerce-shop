f004005812990b984c7aef65b386a18736db5220
Julia Baptista <30318999+juliabaptista@users.noreply.github.com>
2024-01-22 12:50:57 +0100

    [PXZ-000][FIX] @faruka-startsteps comment: unnecessary annotation
    Update ECommerceShopApplication.java

--- src/main/java/de/zalando/ECommerceShopApplication.java
+++ src/main/java/de/zalando/ECommerceShopApplication.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @OpenAPIDefinition(info = @Info(
 		title = "Zalando E-commerce Shop",
 		version = "1.0",
