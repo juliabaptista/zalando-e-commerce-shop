f31155698637ba16178a306fa7aeb0ec54f42b38
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-22 16:58:42 +0100

    [PXZ-000] - ECommerceShopApplication
    Implemente:
    	- Delete unused import statement

--- src/main/java/de/zalando/ECommerceShopApplication.java
+++ src/main/java/de/zalando/ECommerceShopApplication.java
@@ -5,10 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
