a87d95531405bec00eac6ac3859aa76183ec9e8c
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-22 17:38:33 +0100

    [PXZ-32] - JwtAuthenticationFilter
    Refactor:
    	- doFilterInternal: change the implementation logi

--- src/main/java/de/zalando/filter/JwtAuthenticationFilter.java
+++ src/main/java/de/zalando/filter/JwtAuthenticationFilter.java
@@ -31,27 +31,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   ) throws ServletException, IOException {
 
     final String authHeader = request.getHeader("Authorization");
-    final String jwt;
-    final String userEmail;
 
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
-      return;
+    String userEmail = null;
+    String jwt = null;
+
+    if (authHeader != null || authHeader.startsWith("Bearer ")) {
+      jwt = authHeader.substring(7);
+      userEmail = jwtTokenProvider.extractUserEmail(jwt);
     }
-    jwt = authHeader.substring(7);
-    userEmail = jwtTokenProvider.extractUserName(jwt);
+
     if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
       UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
       if (jwtTokenProvider.isTokenValid(jwt, userDetails)) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
             userDetails,
             null,
             userDetails.getAuthorities()
         );
-        authenticationToken.setDetails(
+
+        usernamePasswordAuthenticationToken.setDetails(
             new WebAuthenticationDetailsSource().buildDetails(request)
         );
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
       }
     }
     filterChain.doFilter(request, response);
