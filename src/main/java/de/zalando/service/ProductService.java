515374c65c6bc6edbd9425db8797570168ea61cb
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-25 15:46:53 +0100

    [PXZ-72] Refactor ProductService
    Problem:
            - UnsatisfiedDependencyException: error creating bean
            - no property 'id', 'name', 'methods'  found for type 'Product'
    
    Implement:
            - delete all methods
            - change name of method getProductsById to  getProductByProductId

---
+++ src/main/java/de/zalando/service/ProductService.java
@@ -0,0 +1,40 @@
+package de.zalando.service;
+
+import de.zalando.exception.ProductNotFoundException;
+import de.zalando.model.entities.Product;
+import de.zalando.model.repositories.ProductRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+  ProductRepository productRepository;
+
+  @Autowired
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public Product getProductByProductId(Long productId) throws ProductNotFoundException {
+    Optional<Product> optionalProduct = productRepository.findById(productId);
+    if (optionalProduct.isPresent()) {
+      return optionalProduct.get();
+    } else throw new ProductNotFoundException("Product not found.");
+  }
+
+  //MockData -> saveProduct to save one product
+  public Product saveProduct(Product product) {
+    return productRepository.save(product);
+  }
+
+  //MockData -> saveAll to save a list of products
+  public List<Product> saveAll(List<Product> products) {
+    return productRepository.saveAll(products);
+  }
+}
