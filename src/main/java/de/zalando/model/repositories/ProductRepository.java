9350955003adfb3cad7c777ce0a8fe86a7ae6b40
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-01-25 15:41:17 +0100

    [PXZ-71] - Refactor UserRepository
    Problem:
    	- UnsatisfiedDependencyException: error creating bean
    	- no property 'id', 'name', 'methods'  found for type 'Product'
    
    Implement:
    	- delete all methods
    	- change name of method getProductsById to  getProductByProductId

--- src/main/java/de/zalando/model/repositories/ProductRepository.java
+++ src/main/java/de/zalando/model/repositories/ProductRepository.java
@@ -9,15 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  Optional<Product> getProductById(Long productId);
-  Page<Product> getAllProducts(Pageable pageable);
-  List<Product> getProductByNameIgnoreCase(String productName);
-  Page<Product> getProductByNameIgnoreCase(String productName, Pageable pageable);
-  Optional<Product> getProductByNameAndUserIgnoreCase(String productName, User user);
-  Optional<Product> getProductByIdAndUserIgnoreCase(Integer productId, User user);
-  List<Product> getProductByUser(User user);
-
+  Optional<Product> getProductByProductId(Long productId);
 }
