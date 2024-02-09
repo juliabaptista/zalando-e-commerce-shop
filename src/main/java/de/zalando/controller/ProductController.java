535cae162143a612902a40679aad2e4a19e5ee82
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-09 15:37:03 +0100

    [PXZ-70] Product Controller
    Implement:
    	- pageable getAllByArchivedIsFalse
    Refactor:
            - update getProductByProductId name to getProductByProductIdAndArchivedIsFalse

--- src/main/java/de/zalando/controller/ProductController.java
+++ src/main/java/de/zalando/controller/ProductController.java
@@ -1,15 +1,19 @@
 package de.zalando.controller;
 
 import de.zalando.exception.ProductNotFoundException;
+import de.zalando.model.entities.Product;
 import de.zalando.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/products")
@@ -23,10 +27,18 @@ public class ProductController {
     this.productService = productService;
   }
 
+  @GetMapping
+  public ResponseEntity<Page<Product>> getAllByArchivedIsFalse(
+      @RequestParam("page") int page,
+      @RequestParam("size") int size
+  ) {
+    return ResponseEntity.ok(productService.getAllByArchivedIsFalse(page, size));
+  }
+
   @GetMapping("/{id}")
-  public ResponseEntity<?> getProductByProductId(@PathVariable("id") Long productId) {
+  public ResponseEntity<?> getProductByProductIdAndArchivedIsFalse(@PathVariable("id") Long productId) {
     try {
-      return ResponseEntity.ok(productService.getProductByProductId(productId));
+      return ResponseEntity.ok(productService.getProductByProductIdAndArchivedIsFalse(productId));
     } catch (ProductNotFoundException e){
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
