7426c6da3a75de41c61bb46c2c5fea087378eafb
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-12 19:37:45 +0100

    [PXZ-000] Update MockData

--- src/main/java/de/zalando/MockData.java
+++ src/main/java/de/zalando/MockData.java
@@ -20,9 +20,9 @@ public class MockData {
 
     User adminUser = new User(1L, "Admin first name", "Admin last name", "admin@gmail.com", "123456", Role.ADMIN, false);
 
-    Product product1 = new Product("Product1", "Product1 description", 19.99,10, adminUser );
-    Product product2 = new Product("Product2", "Product2 description", 29.99,20, adminUser );
-    Product product3 = new Product("Product3", "Product3 description", 39.99,30, adminUser );
+    Product product1 = new Product("Product One", "Product One description", 19.99,10, adminUser );
+    Product product2 = new Product("Product Two", "Product Two description", 29.99,20, adminUser );
+    Product product3 = new Product("Product Three", "Product Three description", 39.99,30, adminUser );
     productService.saveAll(List.of(product1, product2, product3));
   }
 }
