cb166b7a84e1a8b125591fe24013b9f72f36cc77
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-15 23:45:07 +0100

    [PXZ-78] CartRepository
    Implement:
    	- query methods getCartItemsByCustomer

--- src/main/java/de/zalando/model/repositories/CartRepository.java
+++ src/main/java/de/zalando/model/repositories/CartRepository.java
@@ -1,10 +1,16 @@
 package de.zalando.model.repositories;
 
 import de.zalando.model.entities.CartItem;
+import de.zalando.model.entities.Product;
+import de.zalando.model.entities.User;
+import java.util.List;
+import java.util.Optional;
+import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartItem, Long> {
-
+  List<CartItem> getCartItemsByCustomer(User user);
+  Optional<CartItem> getCartItemsByCustomerAndProduct(User user, Product product);
 }
