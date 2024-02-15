15018cd3cc9adc67391a37effe7198d50a5a50d4
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-16 00:01:49 +0100

    [PXZ-79] CartController

---
+++ src/main/java/de/zalando/controller/CartController.java
@@ -0,0 +1,42 @@
+package de.zalando.controller;
+
+import de.zalando.exception.ApiError;
+import de.zalando.exception.UserNotFoundException;
+import de.zalando.service.CartService;
+import de.zalando.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+  CartService cartService;
+  UserService userService;
+
+@Autowired
+  public CartController(CartService cartService, UserService userService) {
+    this.cartService = cartService;
+    this.userService = userService;
+  }
+
+  @GetMapping
+  @PreAuthorize("hasRole('CUSTOMER')")
+  public ResponseEntity<?> getCartItemsByCustomer(@AuthenticationPrincipal UserDetails principal) {
+    try {
+      return ResponseEntity.ok(cartService.getCartItemsByCustomer(userService.getUserByEmail(principal.getUsername())));
+    } catch (UserNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+          .body(new ApiError(HttpStatus.NOT_ACCEPTABLE, e.getMessage()));
+    }
+  }
+}
