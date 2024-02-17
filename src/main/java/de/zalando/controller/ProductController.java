58b08e3a83c391272cad3997ff0d107e8fc4d81c
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-17 12:14:28 +0100

    [PXZ-73] Implement POST, PUT and DELETE endpoints
    Implement:
    	- ProductRepository - query methods for ADMIN
    	- ProductController - POST, PUT and DELETE endpoints
    	- ProductService - addProduct, updateProduct and archiveProduct methods
    
    Refactor:
    	- ProductService - to use @RequiredArgsConstructor
    	- ProductController - error message to use ApiError
    	- ProductRequest - add missing annotations

--- src/main/java/de/zalando/controller/ProductController.java
+++ src/main/java/de/zalando/controller/ProductController.java
@@ -1,23 +1,33 @@
 package de.zalando.controller;
 
+import de.zalando.dto.ProductRequest;
+import de.zalando.exception.ApiError;
+import de.zalando.exception.DuplicateProductException;
 import de.zalando.exception.ProductNotFoundException;
+import de.zalando.exception.UserNotFoundException;
 import de.zalando.model.entities.Product;
+import de.zalando.model.entities.User;
 import de.zalando.service.ProductService;
-import java.util.Collections;
-import java.util.List;
+import de.zalando.service.UserService;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/products")
@@ -25,10 +35,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
   ProductService productService;
-
+  UserService userService;
   @Autowired
-  public ProductController(ProductService productService) {
+  public ProductController(ProductService productService, UserService userService) {
     this.productService = productService;
+    this.userService = userService;
   }
 
   @GetMapping
@@ -50,7 +61,8 @@ public class ProductController {
           productService.getAllByArchivedIsFalseAndProductNameContainingIgnoreCase(keyword, page,
               size));
     } catch (ProductNotFoundException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(new ApiError(HttpStatus.NOT_FOUND, e.getMessage()));
     }
   }
 
@@ -60,7 +72,64 @@ public class ProductController {
     try {
       return ResponseEntity.ok(productService.getProductByProductIdAndArchivedIsFalse(productId));
     } catch (ProductNotFoundException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(new ApiError(HttpStatus.NOT_FOUND, e.getMessage()));
     }
+  }
+
+  @PostMapping
+  public ResponseEntity<?> addNewProduct(
+      @Validated @RequestBody ProductRequest productRequest,
+      @AuthenticationPrincipal UserDetails userDetails
+  ) {
+    User user;
+    Product product;
+    try {
+      user = userService.getUserByEmail(userDetails.getUsername());
+      product = productService.addProduct(user, productRequest);
+    } catch (UserNotFoundException | DuplicateProductException e) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+          .body(new ApiError(HttpStatus.NOT_ACCEPTABLE, e.getMessage()));
+    }
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{product-id}")
+        .buildAndExpand(product.getProductId())
+        .toUri();
+    return ResponseEntity.created(uri).body(product);
+  }
+
+  @PutMapping("/{product-id}")
+  public ResponseEntity<?> updateProduct(
+      @PathVariable("product-id") Long productId,
+      @Validated @RequestBody ProductRequest productRequest,
+      @AuthenticationPrincipal UserDetails userDetails
+  ) {
+    User user;
+    Product product;
+    try {
+      user = userService.getUserByEmail(userDetails.getUsername());
+      product = productService.updateProduct(user, productId, productRequest);
+    } catch (UserNotFoundException | ProductNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+          .body(new ApiError(HttpStatus.NOT_ACCEPTABLE, e.getMessage()));
+    }
+    return ResponseEntity.ok(product);
+  }
+
+  @DeleteMapping("/{product-id}")
+  public ResponseEntity<?> deleteProduct(
+      @PathVariable("product-id") Long productId,
+      @AuthenticationPrincipal UserDetails userDetails
+  ) {
+    User user;
+    Product product;
+    try {
+      user = userService.getUserByEmail(userDetails.getUsername());
+      product = productService.archiveProduct(user, productId);
+    } catch (UserNotFoundException | ProductNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+          .body(new ApiError(HttpStatus.NOT_ACCEPTABLE, e.getMessage()));
+    }
+    return ResponseEntity.ok(product);
   }
 }
--- src/main/java/de/zalando/dto/ProductRequest.java
+++ src/main/java/de/zalando/dto/ProductRequest.java
@@ -1,9 +1,19 @@
 package de.zalando.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor @ToString
 public class ProductRequest {
 
   @NotEmpty(message = "Required field. Cannot be empty!")
@@ -11,9 +21,11 @@ public class ProductRequest {
   @NotEmpty(message = "Required field. Cannot be empty!")
   private String productDescription;
 
-  @NotEmpty(message = "Required field. Cannot be empty!")
+  @DecimalMin(value = "0", inclusive = true, message = "Price cannot be negative.")
+  @Digits(integer=7, fraction=3, message = "Invalid price format.")
   private double productPrice;
-  @NotEmpty(message = "Required field. Cannot be empty!")
+
+  @NotNull(message = "Required field. Cannot be null!")
   @Min(value = 0, message = "Stock quantity cannot be negative.")
   @Max(value = 10_000,message = "Max stock quantity is 10,000.")
   private int stockQuantity;
