9e71014876f07b94615e6c70cb3efd759f180b81
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-18 20:56:21 +0100

    [PXZ-81] OrderRepository

---
+++ src/main/java/de/zalando/model/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package de.zalando.model.repositories;
+
+import de.zalando.model.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
