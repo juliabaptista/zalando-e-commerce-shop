7204cc6bd99717113eec5ce1fb9c8ee678f6a670
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-18 17:12:23 +0100

    [PXZ-50] Order Model
    Implement:
    	- create an order model with fields like ID, date, user, total price, and status

---
+++ src/main/java/de/zalando/model/entities/Order.java
@@ -0,0 +1,54 @@
+package de.zalando.model.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Order {
+
+  @Id
+  @Column(name = "order_id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long orderId;
+
+  @Column(name = "order_date")
+  private LocalDateTime orderDate;
+
+  @JsonIgnore
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private User customer;
+
+  @Column(name = "total_price")
+  private double totalPrice;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status")
+  private OrderStatus status;
+
+  public Order(User customer, double totalPrice, OrderStatus status) {
+    this.orderDate = LocalDateTime.now();
+    this.customer = customer;
+    this.totalPrice = totalPrice;
+    this.status = status;
+  }
+}
