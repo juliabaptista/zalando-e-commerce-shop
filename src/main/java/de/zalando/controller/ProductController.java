10e650a926663f636ba056d919ab536ef09aeeac
Julia Baptista <30318999+juliabaptista@users.noreply.github.com>
2024-02-20 22:20:18 +0100

    Update ProductController.java
    [PXZ-000] Solving conflicts

--- src/main/java/de/zalando/controller/ProductController.java
+++ src/main/java/de/zalando/controller/ProductController.java
@@ -1,27 +1,34 @@
 package de.zalando.controller;
 
+import de.zalando.dto.ProductRequest;
+import de.zalando.exception.ApiError;
+import de.zalando.exception.DuplicateProductException;
 import de.zalando.exception.ProductNotFoundException;
+import de.zalando.exception.UserNotFoundException;
 import de.zalando.model.entities.Product;
+import de.zalando.model.entities.User;
 import de.zalando.service.ProductService;
-import java.util.Collections;
-import java.util.List;
+import de.zalando.service.UserService;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/products")
@@ -50,7 +57,8 @@ public class ProductController {
           productService.getAllByArchivedIsFalseAndProductNameContainingIgnoreCase(keyword, page,
               size));
     } catch (ProductNotFoundException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(new ApiError(HttpStatus.NOT_FOUND, e.getMessage()));
     }
   }
 
@@ -60,10 +68,12 @@ public class ProductController {
     try {
       return ResponseEntity.ok(productService.getProductByProductIdAndArchivedIsFalse(productId));
     } catch (ProductNotFoundException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(new ApiError(HttpStatus.NOT_FOUND, e.getMessage()));
     }
   }
 
+  @PreAuthorize("hasRole('ADMIN')")
   @PostMapping
   public ResponseEntity<?> addNewProduct(
       @Validated @RequestBody ProductRequest productRequest,
@@ -85,6 +95,7 @@ public class ProductController {
     return ResponseEntity.created(uri).body(product);
   }
 
+  @PreAuthorize("hasRole('ADMIN')")
   @PutMapping("/{product-id}")
   public ResponseEntity<?> updateProduct(
       @PathVariable("product-id") Long productId,
@@ -103,6 +114,7 @@ public class ProductController {
     return ResponseEntity.ok(product);
   }
 
+  @PreAuthorize("hasRole('ADMIN')")
   @DeleteMapping("/{product-id}")
   public ResponseEntity<?> deleteProduct(
       @PathVariable("product-id") Long productId,
@@ -117,5 +129,6 @@ public class ProductController {
       return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
           .body(new ApiError(HttpStatus.NOT_ACCEPTABLE, e.getMessage()));
     }
+    return ResponseEntity.ok(product);
   }
 }
