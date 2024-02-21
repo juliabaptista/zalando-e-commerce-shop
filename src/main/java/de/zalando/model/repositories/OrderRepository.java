5cd03e7d1d1174901028fc711b689fa5b5334598
Julia Baptista <30318999+juliabaptista@users.noreply.github.com>
2024-02-21 13:49:01 +0100

    Update OrderRepository.java
    [PXZ-000] Solving conflicts

--- src/main/java/de/zalando/model/repositories/OrderRepository.java
+++ src/main/java/de/zalando/model/repositories/OrderRepository.java
@@ -3,16 +3,12 @@ package de.zalando.model.repositories;
 import de.zalando.model.entities.Order;
 import de.zalando.model.entities.OrderStatus;
 import de.zalando.model.entities.User;
-import java.util.List;
 import java.util.Optional;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
