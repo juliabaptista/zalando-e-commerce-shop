edfa5205f5a691ad50e07b6f018b2e16ca557a91
juliabaptista <juliabaptistadeoliveira@gmail.com>
2024-02-22 12:51:16 +0100

    [PXZ-000] Solving database error when creating user table when starting the application

--- src/main/java/de/zalando/model/entities/User.java
+++ src/main/java/de/zalando/model/entities/User.java
@@ -14,17 +14,24 @@ public class User {
   @Column(name = "user_id")
   @GeneratedValue (strategy = GenerationType.AUTO)
   private Long userId;
+
   @Column(name = "first_name")
   private String firstName;
+
   @Column(name = "last_name")
   private String lastName;
+
   @Column(name = "email")
   private String email;
+
   @JsonIgnore
+  @Column(name = "password")
   private String password;
-  @Enumerated
+
+  @Enumerated(EnumType.STRING)
   @Column(name = "role")
   private Role role;
-  @Column(name = "is_archived", columnDefinition = "false")
+
+  @Column(name = "is_archived")
   private boolean archived;
 }
